4c0342ba042c869fcfed0cec81d912f04ac398a0
akhatarmourad <mourad.akhatar01@gmail.com>
2024-09-27 16:43:25 +0100

    Order fetched results

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,13 @@ export default function Home() {
 
   const [fetchError, setFetchError] = useState(null);
   const [data, setData] = useState(null);
+  const [orderBy, setOrderBy] = useState('created_at');
 
 
   // Fetch Data 
   useEffect(() => {
     const fetchData = async () => {
-      const { data, error } = await supabase.from("Features").select();
+      const { data, error } = await supabase.from("Features").select().order(orderBy, {ascending: false});
 
       if(error) {
         setFetchError("Could not fetch data !");
@@ -26,7 +27,7 @@ export default function Home() {
 
     fetchData();
     
-  }, []);
+  }, [orderBy]);
 
   // Update Local State when deleting an item
   const handleDelete = (id) => {
@@ -40,14 +41,22 @@ export default function Home() {
     <div>
         {fetchError && <p className="bg-red-200 border border-red-300 text-red-700 rounded-lg p-3 text-center my-5">{fetchError}</p>}
         {data && (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mt-10 overflow-x-auto">
-            {
-              data.map((item) => {
-                return (
-                  <FeatureCard item={item} key={item.id} onDelete={handleDelete} />
-                );
-              })
-            }
+          <div className="mt-10 overflow-x-auto">
+            <div className="flex items-center justify-center gap-5 mb-5">
+              <button onClick={() => setOrderBy("created_at")} className={`${orderBy === 'created_at' ? 'bg-sky-500 text-white' : 'bg-white'} px-2 py-1 rounded-lg text-slate-500`}>Creation Date</button>
+              <button onClick={() => setOrderBy("rating")} className={`${orderBy === 'rating' ? 'bg-sky-500 text-white' : 'bg-white'} px-2 py-1 rounded-lg text-slate-500`}>Rating</button>
+              <button onClick={() => setOrderBy("title")} className={`${orderBy === 'title' ? 'bg-sky-500 text-white' : 'bg-white'} px-2 py-1 rounded-lg text-slate-500`}>Title</button>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+              {
+                data.map((item) => {
+                  return (
+                    <FeatureCard item={item} key={item.id} onDelete={handleDelete} />
+                  );
+                })
+              }
+            </div>
           </div>
         )}
     </div>
